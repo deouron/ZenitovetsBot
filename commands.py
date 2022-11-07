adfe3e2eb48e5a750133a2bf9e4fe7ecf3b74c83
Dmitriy Sidorov <dima.sidorov.2003@list.ru>
2022-11-07 21:10:13 +0300

    changing send_statistics

--- commands.py
+++ commands.py
@@ -29,9 +29,10 @@ def promote_user(bot, message):
 
 
 def send_statistics(bot, message):
-    bot.send_message(message.chat.id, text='Участников в чате: '
-                                           + str(bot.get_chat_member_count(message.chat.id))
-                                           + '\nАдминов в чате: ' + utils.count_admins(bot, message))
+    bot.send_message(message.chat.id, text='Участников в чате (без ботов): ' + \
+                                           utils.count_users(bot, message) + \
+                                           '\nАдминов в чате (без ботов): ' + \
+                                           utils.count_admins(bot, message))
 
 
 def leave_chat(bot, message):
